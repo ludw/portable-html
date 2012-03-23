e41ca238688ff9c3f5092657ad76e149fd71b7e6
Ludvig Widman <ludvig.widman@codemill.se>
2012-03-23 16:38:52 +0100

    First commit

---
+++ example-source.txl
@@ -0,0 +1,9 @@
+title=Somet title
+template=default.html
+
+h1. This is a example
+
+Hello
+this is a *example*
+
+!image.png!
---
+++ ptm.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import sys
+import textile
+import re
+from jinja2 import Environment, PackageLoader
+
+def read_infile(path):
+    infile = open(path)
+    text = infile.read()
+    infile.close()
+    return text
+
+def parse_infile(infile):
+    config = {'template': 'default.html'}
+    text = ""
+    for line in infile.split("\n"):
+        if re.match(r"^[^=]+=[^=]+$", line):
+            key, val = line.split('=')
+            config[key] = val
+        else:
+            text += line+"\n"
+
+    return config, text
+
+def write_output(path, data):
+    out = open(path, 'w')
+    out.write(data)
+    out.close()
+
+def main(args):
+    if(len(args) != 2):
+        print "Usage: ptm source.txl outfile.htm"
+        return
+
+    infile = read_infile(args[0])
+    context, text = parse_infile(infile)
+    context['body'] = textile.textile(text)
+
+    env = Environment(loader=PackageLoader('ptm', 'templates'))
+    template = env.get_template(context['template'])
+    html = template.render(context)
+    
+    write_output(args[1], html)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
